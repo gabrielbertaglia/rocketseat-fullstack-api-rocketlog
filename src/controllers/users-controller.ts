f6bcd1185a214b712b455aef184bdbf959c8c743
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2025-01-07 11:26:50 -0300

    feat: creating prisma database

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt";
+import { z } from "zod";
 
 export class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
+    const hashedPassword = await hash(password, 8);
+
+    return response.json({ message: "ok", hashedPassword });
   }
 }
