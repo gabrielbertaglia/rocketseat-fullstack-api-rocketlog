f7624c6b006b723129bb8227a53f7eb5865a4522
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2025-01-08 09:49:34 -0300

    feat: adding delivery

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,7 +1,22 @@
+import { prisma } from "@/database/prisma";
 import { Request, Response } from "express";
+import z from "zod";
 
 export class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string().min(1),
+    });
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
